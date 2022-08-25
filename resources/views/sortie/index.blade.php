120b03ada9d577a37788ad4b0cfd520db91043ec
ibrandiaye <ibra789ndiaye@gmail.com>
2022-08-25 12:44:45 +0000

    sortie modal error produit admin  fix

--- resources/views/sortie/index.blade.php
+++ resources/views/sortie/index.blade.php
@@ -107,7 +107,9 @@
                                 @endforeach</td>
                                 {{--  <td>{{ $sortie->quantite  * $sortie->prixv }}</td>  --}}
                                 <td>{{ $sortie->facture->depot->nomd }}</td>
-                                @if(Auth::user()->role=='administrateur')<td>{{( $sortie->quantite  * $sortie->prixv) - ($sortie->quantite  * $sortie->produit->prixu) }}</td>@endif
+                                @if(Auth::user()->role=='administrateur')<td>@if(!emmpty($sortie->produit->prixu)){{( $sortie->quantite  * $sortie->prixv) - ($sortie->quantite  * $sortie->produit->prixu) }}
+
+                                    @endif</td>@endif
                                 <td> @if($sortie->facture->chauffeur)
                                     {{ $sortie->facture->chauffeur->nom }}
                                 @endif</td>
